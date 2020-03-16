e941a81b99945f95896d5fce7f427412d99d3ee6
brickmaker <3150104891@zju.edu.cn>
2020-03-16 11:00:16 +0800

    style

--- src/components/WholeGraph.jsx
+++ src/components/WholeGraph.jsx
@@ -7,6 +7,13 @@ import { GraphTransformer } from '../utils/vis'
 const configs = {
     width: 1920,
     height: 1080,
+    bgColor: 'rgba(238, 238, 238, 1)',
+    node: {
+        color: 'rgba(36, 144, 200, 0.5)'
+    },
+    link: {
+        color: 'rgba(153, 153, 153, 0.1)'
+    },
     padding: 20,
 }
 
@@ -37,10 +44,11 @@ class WholeGraph extends React.Component {
         svg
             .attr('width', configs.width)
             .attr('height', configs.height)
+            .style('background', configs.bgColor)
             .attr('viewBox', [0, 0, configs.width, configs.height]);
 
         const link = svg.append("g")
-            .attr("stroke", "#999")
+            .attr("stroke", configs.link.color)
             .attr("stroke-opacity", 0.6)
             .selectAll("line")
             .data(links)
@@ -61,7 +69,7 @@ class WholeGraph extends React.Component {
             .attr("r", 5)
             .attr('cx', d => d.x)
             .attr('cy', d => d.y)
-            .attr("fill", '#aaa')
+            .attr("fill", configs.node.color)
     }
 
     render() {
